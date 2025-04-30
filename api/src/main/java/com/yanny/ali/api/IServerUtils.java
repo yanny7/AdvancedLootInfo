df989542b694a1322828296abeaba184af0162bf
Yanny <lalis.jan@gmailcom>
2025-04-30 21:36:44 +0200

    Code cleanup

--- api/src/main/java/com/yanny/ali/api/IServerUtils.java
+++ api/src/main/java/com/yanny/ali/api/IServerUtils.java
@@ -11,7 +11,9 @@ import java.util.List;
 
 public interface IServerUtils extends ICommonUtils {
     <T extends LootPoolEntryContainer> List<Item> collectItems(IServerUtils utils, T entry);
+
     <T extends LootItemCondition> List<Item> collectItems(IServerUtils utils, List<Item> items, T condition);
+
     <T extends LootItemFunction> List<Item> collectItems(IServerUtils utils, List<Item> items, T function);
 
     @Nullable
