c6ba6b1b0c6c120ae5b4780ee0764ef74ccaa15d
Yanny <lalis.jan@gmailcom>
2025-05-19 15:32:27 +0200

    Used new JEI API

--- common-jei/src/main/java/com/yanny/ali/compatibility/jei/JeiGameplayLoot.java
+++ common-jei/src/main/java/com/yanny/ali/compatibility/jei/JeiGameplayLoot.java
@@ -3,31 +3,25 @@ package com.yanny.ali.compatibility.jei;
 import com.yanny.ali.compatibility.common.GameplayLootType;
 import com.yanny.ali.registries.LootCategory;
 import mezz.jei.api.gui.drawable.IDrawable;
-import mezz.jei.api.gui.ingredient.IRecipeSlotsView;
+import mezz.jei.api.gui.ingredient.IRecipeSlotDrawable;
+import mezz.jei.api.gui.widgets.IRecipeExtrasBuilder;
+import mezz.jei.api.gui.widgets.IRecipeWidget;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.RecipeType;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
-import org.jetbrains.annotations.NotNull;
+import oshi.util.tuples.Pair;
+
+import java.util.List;
 
 public class JeiGameplayLoot extends JeiBaseLoot<GameplayLootType, String> {
     public JeiGameplayLoot(IGuiHelper guiHelper, RecipeType<GameplayLootType> recipeType, LootCategory<String> lootCategory, Component title, IDrawable icon) {
         super(guiHelper, recipeType, lootCategory, title, icon);
     }
 
-    @NotNull
     @Override
-    public IDrawable getBackground() {
-        return guiHelper.createBlankDrawable(getWidth(), getHeight());
-    }
-
-    @Override
-    public void draw(GameplayLootType recipe, IRecipeSlotsView recipeSlotsView, GuiGraphics guiGraphics, double mouseX, double mouseY) {
-        super.draw(recipe, recipeSlotsView, guiGraphics, mouseX, mouseY);
-        Component component = Component.translatableWithFallback("ali/loot_table/" + recipe.id().substring(1), recipe.id());
-
-        guiGraphics.drawString(Minecraft.getInstance().font, component, 0, 0, 0, false);
+    Pair<List<IRecipeWidget>, List<IRecipeSlotDrawable>> getWidgets(IRecipeExtrasBuilder builder, GameplayLootType recipe) {
+        Component text = Component.translatableWithFallback("ali/loot_table/" + recipe.id().substring(1), recipe.id());
+        return new Pair<>(List.of(createTextWidget(text, 0, 0, false)), List.of());
     }
 
     @Override
---
+++ common-jei/src/main/java/com/yanny/ali/compatibility/jei/JeiLootSlotWidget.java
@@ -0,0 +1,93 @@
+package com.yanny.ali.compatibility.jei;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.yanny.ali.api.RangeValue;
+import com.yanny.ali.api.Rect;
+import mezz.jei.api.gui.builder.ITooltipBuilder;
+import mezz.jei.api.gui.ingredient.IRecipeSlotDrawable;
+import mezz.jei.api.gui.inputs.RecipeSlotUnderMouse;
+import mezz.jei.api.gui.widgets.ISlottedRecipeWidget;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.navigation.ScreenPosition;
+import net.minecraft.core.Holder;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.enchantment.Enchantment;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class JeiLootSlotWidget implements ISlottedRecipeWidget {
+    private final Rect rect;
+    private final IRecipeSlotDrawable slotDrawable;
+    @Nullable
+    private Component count;
+    private boolean isRange = false;
+
+    public JeiLootSlotWidget(IRecipeSlotDrawable slotDrawable, int x, int y, Map<Holder<Enchantment>, Map<Integer, RangeValue>> count) {
+        this.slotDrawable = slotDrawable;
+        rect = new Rect(x, y, 18, 18);
+        setCount(count.get(null).get(0));
+    }
+
+    @NotNull
+    @Override
+    public Optional<RecipeSlotUnderMouse> getSlotUnderMouse(double mouseX, double mouseY) {
+        if (slotDrawable.isMouseOver(mouseX, mouseY)) {
+            return Optional.of(new RecipeSlotUnderMouse(slotDrawable, 0, 0));
+        }
+
+        return Optional.empty();
+    }
+
+    @NotNull
+    @Override
+    public ScreenPosition getPosition() {
+        return new ScreenPosition(rect.x(), rect.y());
+    }
+
+    @Override
+    public void drawWidget(GuiGraphics guiGraphics, double mouseX, double mouseY) {
+        PoseStack stack = guiGraphics.pose();
+
+        stack.translate(1, 1, 0);
+        slotDrawable.draw(guiGraphics);
+        stack.translate(-1, -1, 0);
+
+        if (count != null) {
+            Font font = Minecraft.getInstance().font;
+
+            stack.pushPose();
+            stack.translate(rect.x(), rect.y(), 0);
+
+            if (isRange) {
+                stack.translate(17, 13, 200);
+                stack.pushPose();
+                stack.scale(0.5f, 0.5f, 0.5f);
+                guiGraphics.drawString(font, count, -font.width(count), 0, 16777215, false);
+                stack.popPose();
+            } else {
+                stack.translate(18, 10, 200);
+                guiGraphics.drawString(font, count, -font.width(count), 0, 16777215, true);
+            }
+
+            stack.popPose();
+        }
+    }
+
+    public void getTooltip(ITooltipBuilder tooltip, double mouseX, double mouseY) {
+        if (slotDrawable.isMouseOver(mouseX, mouseY)) {
+            slotDrawable.getTooltip(tooltip);
+        }
+    }
+
+    private void setCount(RangeValue count) {
+        if (count.isRange() || count.min() > 1) {
+            this.count = Component.literal(count.toIntString());
+            isRange = count.isRange();
+        }
+    }
+}
