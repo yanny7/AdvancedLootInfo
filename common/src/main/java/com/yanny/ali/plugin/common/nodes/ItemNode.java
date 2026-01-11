eaba69a4c7fcbdd0ffe2a951cf511f9a3e95b74c
Yanny <lalis.jan@gmailcom>
2026-01-11 22:46:21 +0100

    Fixed crash caused by invalid data and concurrency

--- common/src/main/java/com/yanny/ali/plugin/common/nodes/ItemNode.java
+++ common/src/main/java/com/yanny/ali/plugin/common/nodes/ItemNode.java
@@ -1,22 +1,27 @@
 package com.yanny.ali.plugin.common.nodes;
 
 import com.mojang.datafixers.util.Either;
+import com.mojang.logging.LogUtils;
 import com.yanny.ali.Utils;
 import com.yanny.ali.api.*;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.storage.loot.functions.LootItemFunction;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import org.slf4j.Logger;
 
 import java.util.Collections;
 import java.util.List;
 
 public class ItemNode implements IDataNode, IItemNode {
     public static final ResourceLocation ID = new ResourceLocation(Utils.MOD_ID, "item");
+    private static final Logger LOGGER = LogUtils.getLogger();
 
     private final ITooltipNode tooltip;
     private final List<LootItemCondition> conditions;
@@ -79,7 +84,16 @@ public class ItemNode implements IDataNode, IItemNode {
 
     @Override
     public void encode(IServerUtils utils, FriendlyByteBuf buf) {
-        buf.writeEither(item, FriendlyByteBuf::writeItem, (b, t) -> b.writeResourceLocation(t.location()));
+        int index = buf.writerIndex();
+
+        try {
+            buf.writeEither(item, FriendlyByteBuf::writeItem, (b, t) -> b.writeResourceLocation(t.location()));
+        } catch (Throwable e) {
+            buf.writerIndex(index);
+            buf.writeEither(Either.left(ItemStack.EMPTY), FriendlyByteBuf::writeItem, (b, t) -> {});
+            LOGGER.warn("Failed to encode {}/{}", BuiltInRegistries.ITEM.getKey(item.left().map(ItemStack::getItem).orElse(Items.AIR)), item.right().orElse(null), e);
+        }
+
         ITooltipNode.encodeNode(utils, tooltip, buf);
         count.encode(buf);
         buf.writeFloat(chance);
