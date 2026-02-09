2f927717ece49fcefe00528a2f482b86bb5afc7e
Yanny <lalis.jan@gmailcom>
2026-02-09 23:25:15 +0100

    Fixes after merge

--- fabric/src/main/java/com/yanny/ali/fabric/FabricClientBusSubscriber.java
+++ fabric/src/main/java/com/yanny/ali/fabric/FabricClientBusSubscriber.java
@@ -1,9 +1,11 @@
 package com.yanny.ali.fabric;
 
+import com.yanny.ali.fabric.pip.BlockPictureInPictureRenderer;
 import com.yanny.ali.manager.PluginManager;
 import com.yanny.ali.network.StartMessage;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.client.rendering.v1.SpecialGuiElementRegistry;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientPacketListener;
@@ -12,6 +14,7 @@ public class FabricClientBusSubscriber {
     public static void registerEvents() {
         ClientPlayConnectionEvents.JOIN.register(FabricClientBusSubscriber::onConnect);
         ClientPlayConnectionEvents.DISCONNECT.register(FabricClientBusSubscriber::onDisconnect);
+        SpecialGuiElementRegistry.register((context) -> new BlockPictureInPictureRenderer(Minecraft.getInstance().renderBuffers().bufferSource()));
     }
 
     private static void onConnect(ClientPacketListener clientPacketListener, PacketSender packetSender, Minecraft minecraft) {
