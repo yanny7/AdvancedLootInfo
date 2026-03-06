24ec55a4d76770ef825b53873088d08d7f431ac6
Yanny <lalis.jan@gmailcom>
2026-03-06 20:45:51 +0100

    Fixed GLM tooltip

--- common/src/main/java/com/yanny/ali/plugin/glm/GlobalLootModifierUtils.java
+++ common/src/main/java/com/yanny/ali/plugin/glm/GlobalLootModifierUtils.java
@@ -7,6 +7,7 @@ import com.yanny.ali.api.IOperation;
 import com.yanny.ali.api.IServerUtils;
 import com.yanny.ali.plugin.common.nodes.GlobalLootModifierNode;
 import com.yanny.ali.plugin.common.tooltip.ArrayTooltipNode;
+import com.yanny.ali.plugin.common.tooltip.BranchTooltipNode;
 import com.yanny.ali.plugin.mods.BaseAccessor;
 import com.yanny.ali.plugin.mods.ClassAccessor;
 import com.yanny.ali.plugin.mods.ReflectionUtils;
@@ -181,19 +182,21 @@ public class GlobalLootModifierUtils {
         if (modifier.isLootModifier()) {
             return getLootModifier(modifier.getConditions(), (conditions) -> {
                 ArrayTooltipNode.Builder tooltip = ArrayTooltipNode.array();
-                IKeyTooltipNode fieldsTooltip = utils.getValueTooltip(utils, modifier.getName());
 
                 try {
+                    tooltip.add(BranchTooltipNode.branch().add(TooltipUtils.getJsonTooltip(utils, modifier.serialize())).build("ali.util.advanced_loot_info.auto_detected"));
                     tooltip.add(TooltipUtils.getJsonTooltip(utils, modifier.serialize()));
                 } catch (Throwable e) {
                     if (utils.getConfiguration().logMoreStatistics) {
                         LOGGER.warn("Failed to get GLM info from serialized data for {}", modifier.getName(), e);
                     }
 
+                    IKeyTooltipNode fieldsTooltip = utils.getValueTooltip(utils, modifier.getName());
+
                     TooltipUtils.addObjectFields(utils, fieldsTooltip, modifier, modifier.getLootModifierClass());
+                    tooltip.add(fieldsTooltip.build("ali.util.advanced_loot_info.auto_detected"));
                 }
 
-                tooltip.add(fieldsTooltip.build("ali.util.advanced_loot_info.auto_detected"));
                 tooltip.add(GenericTooltipUtils.getConditionsTooltip(utils, conditions));
                 return List.of(new IOperation.AddOperation((i) -> true, new GlobalLootModifierNode(tooltip.build())));
             }, predicate);
