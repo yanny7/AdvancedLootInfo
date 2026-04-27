d65e15088cd0958d695cbc86ef5e1e28ed392fba
Yanny <lalis.jan@gmailcom>
2026-04-27 18:11:59 +0200

    Block and Entity categories use tags/blocks/entityTypes instead of classes for filtering

--- ali/common/src/main/java/com/yanny/ali/configuration/BlockLootCategory.java
+++ ali/common/src/main/java/com/yanny/ali/configuration/BlockLootCategory.java
@@ -3,8 +3,11 @@ package com.yanny.ali.configuration;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
+import com.mojang.datafixers.util.Either;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.util.GsonHelper;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.crafting.Ingredient;
@@ -13,35 +16,60 @@ import net.minecraft.world.level.block.Block;
 import java.util.List;
 
 public class BlockLootCategory extends LootCategory<Block> {
-    private final List<Class<?>> classes;
+    private final List<Either<TagKey<Block>, Block>> blocks;
 
-    public BlockLootCategory(ResourceLocation key, Item icon, boolean hide, List<Ingredient> catalysts, List<Class<?>> classes) {
+    public BlockLootCategory(ResourceLocation key, Item icon, boolean hide, List<Ingredient> catalysts, List<Either<TagKey<Block>, Block>> blocks) {
         super(key, icon, Type.BLOCK, hide, catalysts);
-        this.classes = classes;
+        this.blocks = blocks;
     }
 
     public BlockLootCategory(JsonObject object) {
         super(Type.BLOCK, object);
-        //noinspection unchecked
-        classes = (List<Class<?>>) (Object) GsonHelper.getAsJsonArray(object, "classes").asList().stream().map(JsonElement::getAsString).map((String className) -> {
-            try {
-                return Class.forName(className);
-            } catch (ClassNotFoundException e) {
-                throw new JsonParseException("Failed to resolve class " + className);
-            }
-        }).toList();
+        blocks = GsonHelper.getAsJsonArray(object, "blocks")
+                .asList()
+                .stream()
+                .map(JsonElement::getAsString)
+                .map((s) -> {
+                    if (s.startsWith("#")) {
+                        ResourceLocation location = ResourceLocation.tryParse(s.substring(1));
+
+                        if (location != null) {
+                            return Either.<TagKey<Block>, Block>left(TagKey.create(Registries.BLOCK, location));
+                        }
+                    } else {
+                        ResourceLocation location = ResourceLocation.tryParse(s);
+
+                        if (location != null && BuiltInRegistries.BLOCK.containsKey(location)) {
+                            return Either.<TagKey<Block>, Block>right(BuiltInRegistries.BLOCK.get(location));
+                        }
+                    }
+
+                    return null;
+                })
+                .toList();
     }
 
     @Override
     protected void toJson(JsonObject object) {
         JsonArray array = new JsonArray();
 
-        classes.forEach((c) -> array.add(c.getName()));
-        object.add("classes", array);
+        for (Either<TagKey<Block>, Block> entry : blocks) {
+            entry.ifLeft((tag) -> array.add("#" + tag.location()));
+            entry.ifRight((block) -> array.add(BuiltInRegistries.BLOCK.getKey(block).toString()));
+        }
+
+        object.add("blocks", array);
     }
 
     @Override
     public boolean validate(Block block) {
-        return classes.stream().anyMatch((p) -> p.isAssignableFrom(block.getClass()));
+        if (blocks.isEmpty()) {
+            return true;
+        }
+
+        return blocks.stream().anyMatch((either) -> either.map(
+                (tag) -> block.builtInRegistryHolder().is(tag),
+                (bl) -> bl == block
+        ));
     }
 }
--- ali/common/src/main/java/com/yanny/ali/configuration/EntityLootCategory.java
+++ ali/common/src/main/java/com/yanny/ali/configuration/EntityLootCategory.java
@@ -3,8 +3,11 @@ package com.yanny.ali.configuration;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
+import com.mojang.datafixers.util.Either;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.util.GsonHelper;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
@@ -13,35 +16,60 @@ import net.minecraft.world.item.crafting.Ingredient;
 import java.util.List;
 
 public class EntityLootCategory extends LootCategory<EntityType<?>> {
-    private final List<Class<?>> classes;
+    private final List<Either<TagKey<EntityType<?>>, EntityType<?>>> entityTypes;
 
-    public EntityLootCategory(ResourceLocation key, Item icon, boolean hide, List<Ingredient> catalysts, List<Class<?>> classes) {
+    public EntityLootCategory(ResourceLocation key, Item icon, boolean hide, List<Ingredient> catalysts, List<Either<TagKey<EntityType<?>>, EntityType<?>>> entityTypes) {
         super(key, icon, Type.ENTITY, hide, catalysts);
-        this.classes = classes;
+        this.entityTypes = entityTypes;
     }
 
     public EntityLootCategory(JsonObject object) {
         super(Type.ENTITY, object);
-        //noinspection unchecked
-        classes = (List<Class<?>>) (Object) GsonHelper.getAsJsonArray(object, "classes").asList().stream().map(JsonElement::getAsString).map((String className) -> {
-            try {
-                return Class.forName(className);
-            } catch (ClassNotFoundException e) {
-                throw new JsonParseException("Failed to resolve class " + className);
-            }
-        }).toList();
+        entityTypes = GsonHelper.getAsJsonArray(object, "entityTypes")
+                .asList()
+                .stream()
+                .map(JsonElement::getAsString)
+                .map((s) -> {
+                    if (s.startsWith("#")) {
+                        ResourceLocation location = ResourceLocation.tryParse(s.substring(1));
+
+                        if (location != null) {
+                            return Either.<TagKey<EntityType<?>>, EntityType<?>>left(TagKey.create(Registries.ENTITY_TYPE, location));
+                        }
+                    } else {
+                        ResourceLocation location = ResourceLocation.tryParse(s);
+
+                        if (location != null && BuiltInRegistries.BLOCK.containsKey(location)) {
+                            return Either.<TagKey<EntityType<?>>, EntityType<?>>right(BuiltInRegistries.ENTITY_TYPE.get(location));
+                        }
+                    }
+
+                    return null;
+                })
+                .toList();
     }
 
     @Override
     protected void toJson(JsonObject object) {
         JsonArray array = new JsonArray();
 
-        classes.forEach((c) -> array.add(c.getName()));
-        object.add("classes", array);
+        for (Either<TagKey<EntityType<?>>, EntityType<?>> entry : entityTypes) {
+            entry.ifLeft((tag) -> array.add("#" + tag.location()));
+            entry.ifRight((entityType) -> array.add(BuiltInRegistries.ENTITY_TYPE.getKey(entityType).toString()));
+        }
+
+        object.add("entityTypes", array);
     }
 
     @Override
     public boolean validate(EntityType<?> entityType) {
-        return classes.stream().anyMatch((p) -> p.isAssignableFrom(entityType.getBaseClass()));
+        if (entityTypes.isEmpty()) {
+            return true;
+        }
+
+        return entityTypes.stream().anyMatch((either) -> either.map(
+                (tag) -> entityType.builtInRegistryHolder().is(tag),
+                (et) -> et == entityType
+        ));
     }
 }
