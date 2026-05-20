a44063fdeb243a9ee8def3b68fd9e018b8dd007e
Yanny <lalis.jan@gmailcom>
2026-05-20 18:22:22 +0200

    Fixed missing keyed array

--- ali/common/src/main/java/com/yanny/ali/plugin/server/ConsumeEffectTooltipUtils.java
+++ ali/common/src/main/java/com/yanny/ali/plugin/server/ConsumeEffectTooltipUtils.java
@@ -13,31 +13,29 @@ public class ConsumeEffectTooltipUtils {
         return TooltipBuilder.array((b) -> {
             b.add(utils.getValueTooltip(utils, effect.effects()).build(Lang.Branch.EFFECTS));
             b.add(utils.getValueTooltip(utils, effect.probability()).build(Lang.Value.PROBABILITY));
-        }).key(Lang.ConsumeEffects.APPLY_EFFECTS);
+        }, Lang.ConsumeEffects.APPLY_EFFECTS);
     }
 
     @NotNull
     public static TooltipBuilder getRemoveEffectsTooltip(IServerUtils utils, RemoveStatusEffectsConsumeEffect effect) {
-        return utils.getValueTooltip(utils, effect.effects()).key(Lang.ConsumeEffects.REMOVE_EFFECTS);
+        return TooltipBuilder.array((b) -> b.add(utils.getValueTooltip(utils, effect.effects())), Lang.ConsumeEffects.REMOVE_EFFECTS);
     }
 
     @Unmodifiable
     @NotNull
     public static TooltipBuilder getClearAllEffectsTooltip(IServerUtils ignoredUtils, ConsumeEffect ignoredEEffect) {
-        return TooltipBuilder.keyOnly(Lang.ConsumeEffects.CLEAR_ALL_EFFECTS);
+        return TooltipBuilder.array(TooltipBuilder::showEmpty, Lang.ConsumeEffects.CLEAR_ALL_EFFECTS);
     }
 
     @Unmodifiable
     @NotNull
     public static TooltipBuilder getTeleportRandomlyTooltip(IServerUtils utils, TeleportRandomlyConsumeEffect effect) {
-        return TooltipBuilder.array((b) -> b.add(utils.getValueTooltip(utils, effect.diameter()).build(Lang.Value.DIAMETER)))
-                .key(Lang.ConsumeEffects.TELEPORT_RANDOMLY);
+        return TooltipBuilder.array((b) -> b.add(utils.getValueTooltip(utils, effect.diameter()).build(Lang.Value.DIAMETER)), Lang.ConsumeEffects.TELEPORT_RANDOMLY);
     }
 
     @Unmodifiable
     @NotNull
     public static TooltipBuilder getPlaySoundTooltip(IServerUtils utils, PlaySoundConsumeEffect effect) {
-        return TooltipBuilder.array((b) -> b.add(utils.getValueTooltip(utils, effect.sound()).build(Lang.Value.SOUND)))
-                .key(Lang.ConsumeEffects.PLAY_SOUND);
+        return TooltipBuilder.array((b) -> b.add(utils.getValueTooltip(utils, effect.sound()).build(Lang.Value.SOUND)), Lang.ConsumeEffects.PLAY_SOUND);
     }
 }
