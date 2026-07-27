4f52077e387b1ac936cfd6ae791084a962787df9
Yanny <lalis.jan@gmailcom>
2026-07-27 21:30:26 +0200

    Applied improved performance when rendering outside scroll widget area

--- ali/common-jei/src/main/java/com/yanny/ali/jei/compatibility/jei/JeiScrollWidget.java
+++ ali/common-jei/src/main/java/com/yanny/ali/jei/compatibility/jei/JeiScrollWidget.java
@@ -18,6 +18,8 @@ import java.util.List;
 import java.util.Optional;
 
 public class JeiScrollWidget extends AbstractScrollWidget implements IRecipeWidget, IJeiInputHandler, ISlottedRecipeWidget {
+    private static final int SLOT_SIZE = 18;
+
     private final List<IRecipeWidget> widgets;
 
     public JeiScrollWidget(Rect rect, int contentHeight, List<IRecipeWidget> widgets) {
@@ -63,12 +65,8 @@ public class JeiScrollWidget extends AbstractScrollWidget implements IRecipeWidg
     @Override
     public void renderWidgets(GuiGraphics guiGraphics, double mouseX, double mouseY) {
         for (IRecipeWidget widget : widgets) {
-            if (widget instanceof JeiLootSlotWidget) {
-                boolean tooHigh = widget.getPosition().y() + 18 < scrollOffsetY * contentRect.height() - scrollRect.height() * scrollOffsetY;
-                boolean tooLow = widget.getPosition().y() > scrollOffsetY * contentRect.height() + scrollRect.height() - (scrollOffsetY*scrollRect.height());
-                if (tooHigh || tooLow) {
-                    continue;
-                }
+            if (widget instanceof JeiLootSlotWidget && isOutsideViewport(widget.getPosition().y(), SLOT_SIZE)) {
+                continue;
             }
 
             widget.drawWidget(guiGraphics, mouseX, mouseY);
