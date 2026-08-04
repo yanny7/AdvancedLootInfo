8ad89e605e4118706b47470b124291eb50e1019e
Yanny <lalis.jan@gmailcom>
2026-08-04 21:53:20 +0200

    Fixes after merge

--- awi/neoforge/src/main/java/com/yanny/awi/neoforge/platform/NeoForgeClientPlatformHelper.java
+++ awi/neoforge/src/main/java/com/yanny/awi/neoforge/platform/NeoForgeClientPlatformHelper.java
@@ -2,11 +2,11 @@ package com.yanny.awi.neoforge.platform;
 
 import com.yanny.awi.pip.BlockRenderState;
 import com.yanny.awi.platform.services.IClientPlatformHelper;
-import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
 
 public class NeoForgeClientPlatformHelper implements IClientPlatformHelper {
     @Override
-    public void renderBlockInGui(GuiGraphics guiGraphics, BlockRenderState renderState) {
+    public void renderBlockInGui(GuiGraphicsExtractor guiGraphics, BlockRenderState renderState) {
         guiGraphics.submitPictureInPictureRenderState(renderState);
     }
 }
