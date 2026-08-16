4e6af0550b8d59d2d174c33f315800682c16ba5d
Yanny <lalis.jan@gmailcom>
2026-08-16 21:13:36 +0200

    Fixes after merge

--- ali/common/src/test/java/com/yanny/ali/test/TooltipTestSuite.java
+++ ali/common/src/test/java/com/yanny/ali/test/TooltipTestSuite.java
@@ -1,19 +1,20 @@
 package com.yanny.ali.test;
 
 import com.mojang.datafixers.util.Either;
+import com.mojang.logging.LogUtils;
 import com.mojang.serialization.Codec;
 import com.yanny.aci.api.RangeValue;
+import com.yanny.aci.test.utils.TestUtils;
 import com.yanny.aci.tooltip.TooltipBuilder;
 import com.yanny.aci.tooltip.TooltipNodePalette;
 import com.yanny.ali.api.IServerRegistry;
 import com.yanny.ali.api.IServerUtils;
 import com.yanny.ali.configuration.AliConfig;
+import com.yanny.ali.datagen.LanguageHolder;
 import com.yanny.ali.manager.PluginManager;
 import com.yanny.ali.plugin.server.EnchantedRanges;
 import com.yanny.ali.plugin.server.LootConditionTypes;
 import com.yanny.ali.plugin.server.LootFunctionTypes;
-import com.yanny.ali.datagen.LanguageHolder;
-import com.yanny.aci.test.utils.TestUtils;
 import net.minecraft.DetectedVersion;
 import net.minecraft.SharedConstants;
 import net.minecraft.advancements.predicates.entity.EntitySubPredicate;
