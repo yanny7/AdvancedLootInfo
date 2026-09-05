347fdafdb9590de30bf65f8d709f8987e41dcc8b
Yanny <lalis.jan@gmailcom>
2026-09-05 23:18:05 +0200

    Added Bighorn Sheep variant resolver

--- alicompat/forge/src/compat/twilightforest/java/com/yanny/alicompat/compat/twilightforest/TwilightForestCompat.java
+++ alicompat/forge/src/compat/twilightforest/java/com/yanny/alicompat/compat/twilightforest/TwilightForestCompat.java
@@ -1,13 +1,24 @@
 package com.yanny.alicompat.compat.twilightforest;
 
+import com.yanny.aci.CommonLogUtils;
 import com.yanny.aci.tooltip.TooltipBuilder;
+import com.yanny.ali.api.ICommonRegistry;
 import com.yanny.ali.api.IServerRegistry;
 import com.yanny.ali.plugin.glm.Destination;
 import com.yanny.ali.plugin.glm.IGlobalLootModifierPlugin;
 import com.yanny.alicompat.IGlmModCompat;
+import com.yanny.alicompat.Utils;
 import com.yanny.alicompat.accessor.GlmAccessorUtils;
 import com.yanny.alicompat.accessor.ReflectionUtils;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import twilightforest.entity.passive.Bighorn;
+import twilightforest.init.TFEntities;
 import twilightforest.loot.conditions.GiantPickUsedCondition;
 import twilightforest.loot.conditions.IsMinionCondition;
 import twilightforest.loot.conditions.ModExistsCondition;
@@ -16,13 +27,22 @@ import twilightforest.loot.functions.ModItemSwap;
 import twilightforest.loot.modifiers.FieryToolSmeltingModifier;
 import twilightforest.loot.modifiers.GiantToolGroupingModifier;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class TwilightForestCompat implements IGlmModCompat {
+    private static final Logger LOGGER = CommonLogUtils.getLogger(Utils.MOD_ID);
+
     @NotNull
     @Override
     public String targetModId() {
         return TwilightForestLang.MOD_ID;
+    }
+
+    @Override
+    public void registerCommon(ICommonRegistry registry) {
+        registry.registerEntityVariants(TFEntities.BIGHORN_SHEEP.get(), TwilightForestCompat::getBighornVariants);
     }
 
     @Override
@@ -48,6 +68,40 @@ public class TwilightForestCompat implements IGlmModCompat {
     public void registerGlobalLootModifier(IGlobalLootModifierPlugin.IRegistry registry) {
         GlmAccessorUtils.registerGlobalLootModifier(registry, FieryToolSmeltingModifier.class, FieryToolSmeltingModifierAccessor.class);
         GlmAccessorUtils.registerGlobalLootModifier(registry, GiantToolGroupingModifier.class, GiantToolGroupingModifierAccessor.class);
+    }
+
+    @NotNull
+    private static List<Entity> getBighornVariants(Level level) {
+        EntityType<Bighorn> type = TFEntities.BIGHORN_SHEEP.get();
+        List<Entity> entities = new ArrayList<>();
+
+        for (DyeColor color : DyeColor.values()) {
+            Bighorn bighorn = createBighorn(type, level);
+
+            if (bighorn != null) {
+                bighorn.setColor(color);
+                entities.add(bighorn);
+            }
+        }
+
+        Bighorn bighorn = createBighorn(type, level);
+
+        if (bighorn != null) {
+            bighorn.setSheared(true);
+            entities.add(bighorn);
+        }
+
+        return entities;
+    }
+
+    @Nullable
+    private static Bighorn createBighorn(EntityType<Bighorn> type, Level level) {
+        try {
+            return type.create(level);
+        } catch (Throwable e) {
+            LOGGER.warn("Failed to create bighorn sheep: {}", e.getMessage(), e);
+            return null;
+        }
     }
 
     @NotNull
