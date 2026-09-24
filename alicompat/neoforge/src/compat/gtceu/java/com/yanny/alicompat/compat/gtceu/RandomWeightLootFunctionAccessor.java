1f96db062dc3238c01e485b039b00630786bc189
Yanny <lalis.jan@gmailcom>
2026-09-24 22:16:34 +0200

    Updated supported mods

---
+++ alicompat/neoforge/src/compat/gtceu/java/com/yanny/alicompat/compat/gtceu/RandomWeightLootFunctionAccessor.java
@@ -0,0 +1,69 @@
+package com.yanny.alicompat.compat.gtceu;
+
+import com.gregtechceu.gtceu.data.loot.ChestGenHooks;
+import com.yanny.aci.api.RangeValue;
+import com.yanny.aci.tooltip.TooltipBuilder;
+import com.yanny.ali.api.IServerUtils;
+import com.yanny.ali.language.Lang;
+import com.yanny.ali.plugin.server.EnchantedRanges;
+import com.yanny.alicompat.accessor.BaseAccessor;
+import com.yanny.alicompat.accessor.FieldAccessor;
+import com.yanny.alicompat.accessor.ICountModifier;
+import com.yanny.alicompat.accessor.IFunctionTooltip;
+import com.yanny.alicompat.accessor.IItemStackModifier;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+public class RandomWeightLootFunctionAccessor extends BaseAccessor<ChestGenHooks.RandomWeightLootFunction> implements IFunctionTooltip, ICountModifier, IItemStackModifier {
+    @FieldAccessor
+    private ItemStack stack;
+
+    @FieldAccessor
+    private int minAmount;
+
+    @FieldAccessor
+    private int maxAmount;
+
+    public RandomWeightLootFunctionAccessor(ChestGenHooks.RandomWeightLootFunction parent) {
+        super(parent);
+    }
+
+    @NotNull
+    @Override
+    public TooltipBuilder getTooltip(IServerUtils utils) {
+        return TooltipBuilder.array((b) -> {
+            b.add(utils.getValueTooltip(utils, stack).build(Lang.Branch.ITEM));
+            b.add(utils.getValueTooltip(utils, new RangeValue(minAmount, maxAmount)).build(Lang.Value.COUNT));
+            b.add(utils.getValueTooltip(utils, parent.predicates).build(Lang.Branch.PREDICATES));
+        }, GregTechCEuModernLang.Functions.RANDOM_WEIGHT);
+    }
+
+    @Override
+    public void applyCountModifier(IServerUtils utils, EnchantedRanges count) {
+        count.modifyAllEntries((value) -> getCount());
+    }
+
+    @Override
+    public ItemStack applyItemStackModifier(IServerUtils utils, ItemStack itemStack) {
+        ItemStack newStack = itemStack.copy();
+
+        if (stack.getDamageValue() != 0) {
+            newStack.setDamageValue(stack.getDamageValue());
+        }
+
+        if (!stack.isComponentsPatchEmpty()) {
+            newStack.applyComponents(stack.getComponentsPatch());
+        }
+
+        return newStack;
+    }
+
+    @NotNull
+    private RangeValue getCount() {
+        if (minAmount == maxAmount) {
+            return new RangeValue(minAmount);
+        }
+
+        return new RangeValue(Math.min(minAmount, stack.getMaxStackSize()), Math.min(maxAmount, stack.getMaxStackSize()));
+    }
+}
